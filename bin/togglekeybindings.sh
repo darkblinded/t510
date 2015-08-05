803720c9306f7669490d8104bbdeac807fd447b3
Leonard Frank <dev@leonardfrank.de>
2015-08-05 07:32:22 +0200

    Added functionality to turn menu on and off with keybindings. Workaround, because changing to admin-mode resets the menu. Hopefully I can fix this so the menu will be persistent.

--- bin/togglekeybindings.sh
+++ bin/togglekeybindings.sh
@@ -3,10 +3,12 @@
 if [ -f /writable/home/user/.config/openbox/custom.flag ]
 then
     cp /writable/home/user/.config/openbox/rc.xml.original /writable/home/user/.config/openbox/rc.xml
+    cp /writable/home/user/.config/openbox/menu.xml.original /writable/home/user/.config/openbox/menu.xml
 
     rm /writable/home/user/.config/openbox/custom.flag
 else
     cp /writable/home/user/.config/openbox/rc.xml.custom /writable/home/user/.config/openbox/rc.xml
+    cp /writable/home/user/.config/openbox/menu.xml.custom /writable/home/user/.config/openbox/menu.xml
 
     touch /writable/home/user/.config/openbox/custom.flag
 fi
